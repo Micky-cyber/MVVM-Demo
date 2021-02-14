22ccb4711077a4df4ced3c429752b33a749381cc
Micky-cyber <65967355+Micky-cyber@users.noreply.github.com>
2021-02-15 00:17:49 +0530

    Update ApiConstant.kt

--- DemoMvvmApp/app/src/main/java/com/demomvvmapp/network/ApiConstant.kt
+++ DemoMvvmApp/app/src/main/java/com/demomvvmapp/network/ApiConstant.kt
@@ -2,9 +2,9 @@ package com.demomvvmapp.network
 
 class ApiConstant {
     companion object {
-        const val BASE_URL = "http://157.175.3.212/API/LuxuryQatarApp.php/"
+        const val BASE_URL = ""
         const val SERVICE_URL = "?Service="
-        const val GET_PROMOTED_WATCH_LIST = SERVICE_URL.plus("GetPromotedWatchList")
+        const val GET_PROMOTED_WATCH_LIST = SERVICE_URL.plus("watch")
 
         const val SECRET_KEY: String = "secret_key"
         const val ACCESS_KEY: String = "access_key"
